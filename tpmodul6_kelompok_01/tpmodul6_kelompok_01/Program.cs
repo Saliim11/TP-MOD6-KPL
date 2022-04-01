1f5fd6ecacdda4a79c20a7ed9a18d69a10be236c
RAFI ALIF <rafialif2907@gmail.com>
2022-04-01 17:01:46 +0700

    “menambahkan class DataMahasiswa_1302204116”.

--- tpmodul6_kelompok_01/tpmodul6_kelompok_01/Program.cs
+++ tpmodul6_kelompok_01/tpmodul6_kelompok_01/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace tpmodul6_kelompok_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            DataMahasiswa_1302204116.ReadJson();
         }
     }
 }

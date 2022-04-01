6f5913fad213128f278c0b4f2de47c9d5d5fcf61
rizkyr117h <84673678+rizkyr117h@users.noreply.github.com>
2022-04-01 16:47:31 +0700

    menambahkan class 1302203122

--- tpmodul6_kelompok_01/tpmodul6_kelompok_01/Program.cs
+++ tpmodul6_kelompok_01/tpmodul6_kelompok_01/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace tpmodul6_kelompok_01
 {
@@ -10,6 +13,22 @@ namespace tpmodul6_kelompok_01
     {
         static void Main(string[] args)
         {
+            DataMahasiswa1302203122.ReadJson();
         }
+    }
+    class DataMahasiswa1302203122
+    {
+
+        public static void ReadJson()
+        {
+            //Baca File Json
+            string jsonString = File.ReadAllText("C:/SEMESTER 4/KONSTRUKSI PERANGKAT LUNAK/TP_6/tp6_1_1302203122.json");
+
+            //Desereliase file json menjadi object
+            dynamic mahasiswa = JsonConvert.DeserializeObject(jsonString);
+
+            Console.WriteLine(mahasiswa.nama.depan +" "+ mahasiswa.nama.belakang + " dengan nim " + mahasiswa.nim + " fakultas " + mahasiswa.fakultas);
+        }
+
     }
 }
